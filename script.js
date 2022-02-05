15435bbed1b77796b71b94c7181333a736bda102
Jorge <jsbatalha@gmail.com>
2022-02-05 10:29:23 -0300

    displaying cards randomly

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let cardNum;
-const cards = ["bobrossparrot", "explodyparot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+const cards = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
 startGame();
 
 function startGame(){
@@ -29,17 +29,45 @@ function numCheck (num){
     }
 }
 
+function random() { 
+	return Math.random() - 0.5; 
+}
+
 function displayCards(size){
+    const gameCards = [];
+
+    for(i=0; i<(size/2); i++){
+        for(j=0;j<2;j++){
+            gameCards.push(cards[i]);
+        }
+    }
+
+    gameCards.sort(random);
+
     const local = document.querySelector("main");
 
     for(i=0; i<size; i++){
-        local.innerHTML = local.innerHTML + `<div onclick=(turnCard(this)) class="card"><img class="card-front" src="./Mídias/front.png" alt="Parrot_front"><img class="card-back hidden" src="./Mídias/${cards[i]}.gif" alt=""></div>`
+        local.innerHTML = local.innerHTML + `<div onclick="turnCard(this)" class="card"> <img class="card-front" src="./Mídias/front.png" alt="Card's front face"> <img class="card-back hidden" src="./Mídias/${gameCards[i]}.gif" alt="Card's back face"> </div>`
     }
+    
 }
 
 function turnCard(card){
     const front = card.querySelector(".card-front");
     const back = card.querySelector(".card-back");
     front.classList.toggle("hidden");
-    back.classList.toggle("hidden")
+    back.classList.toggle("hidden");
+}
+
+function checkSide(card){
+    const front = card.querySelector(".card-front");
+
+    if((front.classList.contains("hidden")) === true){
+        console.log("carta aberta");
+        return(true);
+    }
+    else{
+        console.log("carta fechada");
+        return(false);
+    }
 }
