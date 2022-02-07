daf8816900e72a6abe3177d5da33ff8609fc2f30
Jorge <jsbatalha@gmail.com>
2022-02-07 16:47:36 -0300

    Final Project

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let cardNum;
 const cards = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+const gameCards = [];
+let turnedCards = [];
+let turnedLength = 0;
+let cardsLeft = 0;
+let jogadas = 0;
+
 startGame();
 
 function startGame(){
@@ -34,20 +40,21 @@ function random() {
 }
 
 function displayCards(size){
-    const gameCards = [];
+    const local = document.querySelector("main");
+
+    console.log(gameCards);
 
     for(i=0; i<(size/2); i++){
         for(j=0;j<2;j++){
             gameCards.push(cards[i]);
+            gameCards.sort(random);
+            console.log(gameCards);
         }
     }
 
-    gameCards.sort(random);
-
-    const local = document.querySelector("main");
-
     for(i=0; i<size; i++){
-        local.innerHTML = local.innerHTML + `<div onclick="turnCard(this)" class="card"> <img class="card-front" src="./Mídias/front.png" alt="Card's front face"> <img class="card-back hidden" src="./Mídias/${gameCards[i]}.gif" alt="Card's back face"> </div>`
+        cardsLeft++
+        local.innerHTML = local.innerHTML + `<div onclick="turnCard(this)" class="card" id="${gameCards[i]}"> <img class="card-front show" src="./Mídias/front.png" alt="Card's front face"> <img class="card-back hidden" src="./Mídias/${gameCards[i]}.gif" alt="Card's back face"> </div>`
     }
     
 }
@@ -55,19 +62,104 @@ function displayCards(size){
 function turnCard(card){
     const front = card.querySelector(".card-front");
     const back = card.querySelector(".card-back");
-    front.classList.toggle("hidden");
-    back.classList.toggle("hidden");
+
+    const getCards = document.querySelectorAll(`#${turnedCards[0]}`);
+    const cardsInGame = Array.prototype.slice.call(getCards);
+
+    checkSide();
+    if(parseInt(checkSide()) < 2){
+        front.classList.toggle("hidden");
+        back.classList.toggle("hidden");
+        front.classList.toggle("show");
+        back.classList.toggle("show");
+        jogadas++
+    }
+    if(parseInt(checkSide()) === 2){
+        console.log("Tem duas cartas viradas");
+        checkCards();
+    }
+    checkSide();
+    endGame();
 }
 
-function checkSide(card){
-    const front = card.querySelector(".card-front");
+function checkSide(){
+   const getCards = document.querySelectorAll(".card");
+   const cardsInGame = Array.prototype.slice.call(getCards);
+   let selectedCards = 0;
+   turnedCards = [];
 
-    if((front.classList.contains("hidden")) === true){
-        console.log("carta aberta");
-        return(true);
+   for(i=0;i<cardNum;i++){
+        let cardChecking = cardsInGame[i].querySelector(".card-front");
+        let checkHidden = cardChecking.classList.contains("hidden");
+        let checkScore = cardChecking.parentElement.classList.contains("score");
+
+        if(checkHidden === true && checkScore === false){
+            console.log("Carta selecionada")
+            selectedCards ++
+            turnedCards.push(cardsInGame[i].id);
+        }
+        else{
+            console.log("carta não selecionada");
+        }
+   }
+
+   turnedLength = turnedCards.length;
+
+   console.log(selectedCards);
+   return(selectedCards);
+}
+
+function checkCards(){
+    const getCards = document.querySelectorAll(`#${turnedCards[0]}`);
+    const cardsInGame = Array.prototype.slice.call(getCards);
+    checkSide();
+    if(turnedCards[0] === turnedCards[1]){
+        console.log("são iguais")
+
+        cardsLeft = cardsLeft - 2;
+
+        for(i=0;i<2;i++){
+            cardsInGame[i].removeAttribute("onclick");
+            cardsInGame[i].classList.add("score");
+        }
+        
     }
-    else{
-        console.log("carta fechada");
-        return(false);
+    if(turnedCards[0] != turnedCards[1]){
+        console.log("não são iguais");
+
+        setTimeout(restart,1000);
+    }
+}
+
+function restart(){
+    const getCardsFront = document.getElementsByClassName("card-front hidden");
+    const cardsInGameFront = Array.prototype.slice.call(getCardsFront);
+
+    const getCardsBack = document.getElementsByClassName("card-back show");
+    const cardsInGameBack = Array.prototype.slice.call(getCardsBack);
+
+    let length = getCardsBack.length
+
+    console.log(length)
+    console.log(length)
+
+    for(i=0; i<length; i++){
+
+        if(cardsInGameFront[i].parentElement.classList.contains("score") === false){
+
+            console.log("não está marcada")
+            cardsInGameFront[i].classList.toggle("hidden");
+            cardsInGameBack[i].classList.toggle("hidden");
+            cardsInGameFront[i].classList.toggle("show");
+            cardsInGameBack[i].classList.toggle("show");
+        }
+        
+    }
+    
+}
+
+function endGame(){
+    if(cardsLeft == 0){
+        alert(`Voce terminou o jogo em ${jogadas} jogadas`);
     }
 }
